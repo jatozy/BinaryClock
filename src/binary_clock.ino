65ea776e888c22eaacf52aa34442fba8fd801e2f
jatozy <infrastructure@jato-engineering.de>
2021-12-19 16:42:56 +0100

    move implementation of class at the end of the file.

--- src/binary_clock.ino
+++ src/binary_clock.ino
@@ -74,7 +74,8 @@ private:
     static constexpr auto DFC77_SAMPLES_IN_SECOND = 100;
     static constexpr auto DFC77_ZEROS_IN_LINE_FOR_SYNCHRONIZATION = 15;
     static constexpr auto DFC77_IGNORED_SAMPLES_AFTER_SYNCHRONIZATION = 80;
-    static constexpr auto DFC77_NECESSARY_SAMPLES = 40;
+    static constexpr auto DFC77_NECESSARY_SAMPLES = 100;
+    static constexpr auto DFC77_NUMBER_RECEIVED_BITS = 59;
 
 private:
     void printIdleStateOnLeds();
@@ -86,6 +87,8 @@ private:
 
     void synchronizeDcf77(int sample);
     void readAndInterpretDcf77(int sample);
+    void interpretDcf77Second();
+    void interpretAndUseDcf77Minute();
 
 private:
     TimePointReader m_readTimePoint = nullptr;
@@ -99,8 +102,98 @@ private:
     bool m_dcf77IsSynchronized = false;
     uint8_t m_dcf77IgnoreSamples = 0;
     uint8_t m_dcf77SampleCounter = 0;
-    uint8_t m_dcf77Samples[DFC77_NECESSARY_SAMPLES] = {0};
+    uint8_t m_dcf77BitsCounter = 0;
+    uint8_t m_dcf77NumberZerosPerSamplesSecond = 0;
+    uint8_t m_dcf77ReceivedBits[DFC77_NUMBER_RECEIVED_BITS] = {0};
 };
+} // namespace binary_clock
+
+binary_clock::TimePoint readCurrentTimePoint();
+
+RTC_DS3231 rtc;
+unsigned int ledTimer = 0;
+unsigned int dcf77Timer = 0;
+binary_clock::Clock clock(&readCurrentTimePoint, &digitalWrite, digitalRead);
+
+void setup()
+{
+    Serial.begin(9600);
+
+    if (!rtc.begin())
+    {
+        Serial.println("Couldn't find RTC");
+        Serial.flush();
+        abort();
+    }
+
+    Serial.println("Setting the time...");
+    // When time needs to be set on a new device, or after a power loss, the
+    // following line sets the RTC to the date & time this sketch was compiled
+    rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+    // This line sets the RTC with an explicit date & time, for example to set
+    // January 21, 2014 at 3am you would call:
+    // rtc.adjust(DateTime(2014, 1, 21, 3, 0, 0));
+
+    TCCR0A = (1 << WGM01); // Set the CTC mode
+    OCR0A = 0xF9;          // Value for ORC0A for 1ms
+
+    TIMSK0 |= (1 << OCIE0A); // Set the interrupt request
+    sei();                   // Enable interrupt
+
+    TCCR0B |= (1 << CS01); // Set the prescale 1/64 clock
+    TCCR0B |= (1 << CS00);
+
+    pinMode(binary_clock::Clock::SECONDS_GND, OUTPUT);
+    pinMode(binary_clock::Clock::MINUTES_GND, OUTPUT);
+    pinMode(binary_clock::Clock::HOURS_GND, OUTPUT);
+    pinMode(binary_clock::Clock::PIN_1, OUTPUT);
+    pinMode(binary_clock::Clock::PIN_2, OUTPUT);
+    pinMode(binary_clock::Clock::PIN_4, OUTPUT);
+    pinMode(binary_clock::Clock::PIN_8, OUTPUT);
+    pinMode(binary_clock::Clock::PIN_16, OUTPUT);
+    pinMode(binary_clock::Clock::PIN_32, OUTPUT);
+    pinMode(binary_clock::Clock::DCF77_SIGNAL, INPUT_PULLUP);
+
+    digitalWrite(binary_clock::Clock::SECONDS_GND, HIGH);
+    digitalWrite(binary_clock::Clock::MINUTES_GND, HIGH);
+    digitalWrite(binary_clock::Clock::HOURS_GND, HIGH);
+}
+
+void loop()
+{
+    if (ledTimer >= 5)
+    {
+        ledTimer = 0;
+        clock.controllLeds();
+    }
+
+    if (dcf77Timer >= 10)
+    {
+        dcf77Timer = 0;
+        clock.processDcf77Signal();
+    }
+}
+
+ISR(TIMER0_COMPA_vect)
+{
+    ledTimer++;
+    dcf77Timer++;
+}
+
+binary_clock::TimePoint readCurrentTimePoint()
+{
+    const auto now = rtc.now();
+
+    binary_clock::TimePoint result;
+    result.hour.value = now.hour();
+    result.minute.value = now.minute();
+    result.second.value = now.second();
+
+    return result;
+}
+
+namespace binary_clock
+{
 
 Clock::Clock(TimePointReader readTimePoint, PinWriter writePin, PinReader readPin)
     : m_readTimePoint(readTimePoint), m_writePin(writePin), m_readPin(readPin)
@@ -137,15 +230,72 @@ void Clock::readAndInterpretDcf77(int sample)
     if (m_dcf77SampleCounter < DFC77_NECESSARY_SAMPLES)
     {
         Serial.print(sample, DEC);
+        m_dcf77NumberZerosPerSamplesSecond += (sample == 0) ? 1 : 0;
     }
 
     m_dcf77SampleCounter++;
 
     if (m_dcf77SampleCounter >= DFC77_SAMPLES_IN_SECOND)
     {
+        if (m_dcf77NumberZerosPerSamplesSecond == 0)
+        {
+            interpretAndUseDcf77Minute();
+        }
+        else
+        {
+            interpretDcf77Second();
+        }
+
         Serial.print('\n');
+        m_dcf77NumberZerosPerSamplesSecond = 0;
         m_dcf77SampleCounter = 0;
     }
+}
+
+void Clock::interpretDcf77Second()
+{
+    if (m_dcf77BitsCounter >= DFC77_NUMBER_RECEIVED_BITS)
+    {
+        m_dcf77BitsCounter = 0;
+    }
+
+    if (m_dcf77NumberZerosPerSamplesSecond > 15)
+    {
+        m_dcf77ReceivedBits[m_dcf77BitsCounter] = 1;
+    }
+    else
+    {
+        m_dcf77ReceivedBits[m_dcf77BitsCounter] = 0;
+    }
+}
+
+void Clock::interpretAndUseDcf77Minute()
+{
+    int minute = m_dcf77ReceivedBits[21] + (m_dcf77ReceivedBits[22] << 1) +
+                 (m_dcf77ReceivedBits[23] << 2) + (m_dcf77ReceivedBits[24] << 3);
+    minute = minute + ((m_dcf77ReceivedBits[25] + (m_dcf77ReceivedBits[26] << 1) +
+                        (m_dcf77ReceivedBits[27] << 2)) *
+                       10);
+
+    int hour = m_dcf77ReceivedBits[29] + (m_dcf77ReceivedBits[30] << 1) +
+               (m_dcf77ReceivedBits[31] << 2) + (m_dcf77ReceivedBits[32] << 3);
+    hour = hour + ((m_dcf77ReceivedBits[33] + (m_dcf77ReceivedBits[34] << 1)) * 10);
+
+    Serial.print('\t');
+    if (hour < 10)
+    {
+        Serial.print('0');
+    }
+    Serial.print(hour, DEC);
+    Serial.print(':');
+    if (minute < 10)
+    {
+        Serial.print('0');
+    }
+    Serial.print(minute, DEC);
+    Serial.print('\n');
+
+    m_dcf77BitsCounter = 0;
 }
 
 void Clock::synchronizeDcf77(int sample)
@@ -358,87 +508,3 @@ void Clock::printTimePoint(const TimePointValue& value, int groundPin) const
     m_writePin(groundPin, 0);
 }
 } // namespace binary_clock
-
-binary_clock::TimePoint readCurrentTimePoint();
-
-RTC_DS3231 rtc;
-unsigned int ledTimer = 0;
-unsigned int dcf77Timer = 0;
-binary_clock::Clock clock(&readCurrentTimePoint, &digitalWrite, digitalRead);
-
-void setup()
-{
-    Serial.begin(9600);
-
-    if (!rtc.begin())
-    {
-        Serial.println("Couldn't find RTC");
-        Serial.flush();
-        abort();
-    }
-
-    Serial.println("Setting the time...");
-    // When time needs to be set on a new device, or after a power loss, the
-    // following line sets the RTC to the date & time this sketch was compiled
-    rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
-    // This line sets the RTC with an explicit date & time, for example to set
-    // January 21, 2014 at 3am you would call:
-    // rtc.adjust(DateTime(2014, 1, 21, 3, 0, 0));
-
-    TCCR0A = (1 << WGM01); // Set the CTC mode
-    OCR0A = 0xF9;          // Value for ORC0A for 1ms
-
-    TIMSK0 |= (1 << OCIE0A); // Set the interrupt request
-    sei();                   // Enable interrupt
-
-    TCCR0B |= (1 << CS01); // Set the prescale 1/64 clock
-    TCCR0B |= (1 << CS00);
-
-    pinMode(binary_clock::Clock::SECONDS_GND, OUTPUT);
-    pinMode(binary_clock::Clock::MINUTES_GND, OUTPUT);
-    pinMode(binary_clock::Clock::HOURS_GND, OUTPUT);
-    pinMode(binary_clock::Clock::PIN_1, OUTPUT);
-    pinMode(binary_clock::Clock::PIN_2, OUTPUT);
-    pinMode(binary_clock::Clock::PIN_4, OUTPUT);
-    pinMode(binary_clock::Clock::PIN_8, OUTPUT);
-    pinMode(binary_clock::Clock::PIN_16, OUTPUT);
-    pinMode(binary_clock::Clock::PIN_32, OUTPUT);
-    pinMode(binary_clock::Clock::DCF77_SIGNAL, INPUT_PULLUP);
-
-    digitalWrite(binary_clock::Clock::SECONDS_GND, HIGH);
-    digitalWrite(binary_clock::Clock::MINUTES_GND, HIGH);
-    digitalWrite(binary_clock::Clock::HOURS_GND, HIGH);
-}
-
-void loop()
-{
-    if (ledTimer >= 5)
-    {
-        ledTimer = 0;
-        clock.controllLeds();
-    }
-
-    if (dcf77Timer >= 10)
-    {
-        dcf77Timer = 0;
-        clock.processDcf77Signal();
-    }
-}
-
-ISR(TIMER0_COMPA_vect)
-{
-    ledTimer++;
-    dcf77Timer++;
-}
-
-binary_clock::TimePoint readCurrentTimePoint()
-{
-    const auto now = rtc.now();
-
-    binary_clock::TimePoint result;
-    result.hour.value = now.hour();
-    result.minute.value = now.minute();
-    result.second.value = now.second();
-
-    return result;
-}
